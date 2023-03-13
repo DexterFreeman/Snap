665bc0574e868f4c2648923432e6cad2413f056d
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-13 16:20:34 +0000

    Completed stage 3, you can now snap.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        CardGame test = new CardGame();
-        test.getDeckOfCards();
-        test.sortDeckInNumberOrder();
-        System.out.println(test.getDeck());
-        test.sortDeckIntoSuits();
-        System.out.println(test.getDeck());
-        test.shuffleDeck();
-        System.out.println(test.getDeck());
+        Snap.startGame();
     }
 }
--- src/Snap.java
+++ src/Snap.java
@@ -1,2 +1,36 @@
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Snap extends CardGame{
+    public static ArrayList<Card> drawnCards = new ArrayList<>();
+    public static void startGame() {
+        CardGame snap = new CardGame();
+        snap.getDeckOfCards();
+        snap.shuffleDeck();
+        Scanner s = new Scanner(System.in);
+        boolean hasSnap = false;
+        System.out.println("Initial Setup...");
+        System.out.println("Drawing first card:");
+        Card firstCard = snap.dealCard();
+        drawnCards.add(firstCard);
+        System.out.println(drawnCards);
+        while(hasSnap == false) {
+            snap.shuffleDeck();
+            System.out.println("Drawn for turn: ");
+            Card drawnCard = snap.dealCard();
+            drawnCards.add(drawnCard);
+            System.out.println(drawnCards);
+            if(drawnCards.get(drawnCards.size() -1).getSymbol() == drawnCards.get(drawnCards.size() - 2).getSymbol()){
+                hasSnap = true;
+                System.out.printf("Snap! You have won :)");
+            }
+            else{
+                System.out.printf("No Snap, press enter to continue");
+                s.nextLine();
+
+            }
+
+        }
+    }
 }
