b75ff4f61db8961643150489f85bdb6d1b043b57
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-14 16:00:46 +0000

    Users can now type snap in two seconds to win game

--- src/Snap.java
+++ src/Snap.java
@@ -1,14 +1,9 @@
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-
-
 public class Snap extends CardGame{
     public static ArrayList<Card> drawnCards = new ArrayList<>();
-    public static Timer timer = new Timer();
-    public static boolean inputReceived = false;
+
     public static void startGame() {
         boolean isPlayerOneTurn = true;
         CardGame snap = new CardGame();
@@ -32,23 +27,17 @@ public class Snap extends CardGame{
             String cardInfo = drawnCard.toString();
             System.out.println(lastCardDrawnInfo + " | " + cardInfo);
             if(drawnCards.get(drawnCards.size() -1).getSymbol() == drawnCards.get(drawnCards.size() - 2).getSymbol()){
-                timer.schedule(new TimerTask(){
-                    @Override
-                    public void run(){
-                        synchronized (s){
-                            if(!inputReceived){
-                                System.out.println("There was a snap, however you didn't type snap.");
-                            }
-                        }
-                    }
-                }, 2000);
-                String input = s.nextLine();
-                synchronized(s) {
-                    inputReceived = true;
+                long startTime = System.currentTimeMillis();
+                String input = s.nextLine().toLowerCase();
+                long endTime = System.currentTimeMillis();
+                long timeSpentInputting = endTime - startTime;
+                if (input.equals("snap") && timeSpentInputting <= 2000){
+                    hasSnap = true;
+                    System.out.println("Snap! " + (isPlayerOneTurn ? "Player one" : "Player two") + " has won :)");
                 }
-                System.out.println("You entered: " + input);
-                hasSnap = true;
-                System.out.println("Snap! " + (isPlayerOneTurn ? "Player one" : "Player two") + " has won :)");
+                else{
+                    System.out.println("There was a snap, but you either missed it or wasn't quick enough! Continue");
+                }
             }
             else{
                 System.out.printf("No Snap, press enter to continue");
