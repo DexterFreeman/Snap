84d3dba9ee12670e3bffce9ee0f63d9ebdc1c4cb
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-14 16:55:18 +0000

    pushing commits

--- src/Snap.java
+++ src/Snap.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class Snap extends CardGame{
     public static ArrayList<Card> drawnCards = new ArrayList<>();
 
+    int playerTwoWins = 0;
     public static void startGame() {
+
+
         boolean isPlayerOneTurn = true;
         CardGame snap = new CardGame();
         snap.getDeckOfCards();
@@ -16,35 +19,48 @@ public class Snap extends CardGame{
         Card firstCard = snap.dealCard();
         drawnCards.add(firstCard);
         System.out.println(drawnCards);
+        boolean gameOver = false;
+        int playerOneWins = 0;
+        int playerTwoWins = 0;
+        while (gameOver == false) {
 
-        while(hasSnap == false) {
-            isPlayerOneTurn = !isPlayerOneTurn;
-            snap.shuffleDeck();
-            System.out.println("Drawn for turn: ");
-            Card drawnCard = snap.dealCard();
-            String lastCardDrawnInfo = drawnCards.get(drawnCards.size() -1).toString();
-            drawnCards.add(drawnCard);
-            String cardInfo = drawnCard.toString();
-            System.out.println(lastCardDrawnInfo + " | " + cardInfo);
-            if(drawnCards.get(drawnCards.size() -1).getSymbol() == drawnCards.get(drawnCards.size() - 2).getSymbol()){
-                long startTime = System.currentTimeMillis();
-                String input = s.nextLine().toLowerCase();
-                long endTime = System.currentTimeMillis();
-                long timeSpentInputting = endTime - startTime;
-                if (input.equals("snap") && timeSpentInputting <= 2000){
-                    hasSnap = true;
-                    System.out.println("Snap! " + (isPlayerOneTurn ? "Player one" : "Player two") + " has won :)");
+            while (hasSnap == false) {
+                isPlayerOneTurn = !isPlayerOneTurn;
+                snap.shuffleDeck();
+                System.out.println("Drawn for turn: ");
+                Card drawnCard = snap.dealCard();
+                String lastCardDrawnInfo = drawnCards.get(drawnCards.size() - 1).toString();
+                drawnCards.add(drawnCard);
+                String cardInfo = drawnCard.toString();
+                System.out.println(lastCardDrawnInfo + " | " + cardInfo);
+                if (drawnCards.get(drawnCards.size() - 1).getSymbol() == drawnCards.get(drawnCards.size() - 2).getSymbol()) {
+                    long startTime = System.currentTimeMillis();
+                    String input = s.nextLine().toLowerCase();
+                    long endTime = System.currentTimeMillis();
+                    long timeSpentInputting = endTime - startTime;
+                    if (input.equals("snap") && timeSpentInputting <= 2000) {
+                        hasSnap = true;
+                        if(isPlayerOneTurn){
+                            playerOneWins++;
+                        }
+                        else {
+                            playerTwoWins++;
+                        }
+                        System.out.println("Snap! " + (isPlayerOneTurn ? "Player one" : "Player two") + " has won :)");
+                    } else {
+                        System.out.println("There was a snap, but you either missed it or wasn't quick enough! Continue");
+                    }
+                } else {
+                    System.out.printf("No Snap, press enter to continue");
+
+                    s.nextLine();
+
                 }
-                else{
-                    System.out.println("There was a snap, but you either missed it or wasn't quick enough! Continue");
-                }
-            }
-            else{
-                System.out.printf("No Snap, press enter to continue");
-
-                s.nextLine();
 
             }
+            System.out.println("");
+
+
 
         }
     }
